f43530d6d38f2b7b0f2db8153c8d93b1edef412c
VINOSLADENO <vinosladeno@gmail.com>
2026-06-27 15:08:32 +0200

    Implement navbar scroll effect
    Add scroll event listener to toggle navbar class.

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 80){
+
+        navbar.classList.add("scrolled");
+
+    }else{
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
